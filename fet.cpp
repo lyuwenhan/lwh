578328dbb4fbc42131c32a6add6dcd5fddd8e58a
LvWenHan <lyuwenhan@gmail.com>
2021-03-20 13:15:03 +0800

    Create fet.cpp

---
+++ fet.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	double m, h;
+	cin >> m >> h;
+	double a = m / h 2 (40 <= m <= 120,1.4 <= h <= 2.0 );
+	cout << "Today, I ate " << n << " apple";
+	if(n > 1){
+		cout << "s"; 
+	}
+	cout << "." << endl;
+	return 0;
+}
